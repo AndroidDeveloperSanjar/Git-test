bda15cba226b133d1779ff580f5ee919d1f3a3e4
AndroidDeveloperSanjar <sanjarkarimov1996@mail.ru>
2022-11-03 11:49:27 +0500

    test 1 a

--- app/src/main/java/uz/androbeck/gittest/MainActivity.kt
+++ app/src/main/java/uz/androbeck/gittest/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //sanjar test 1
+        //sanjar test 1 a
     }
 }
